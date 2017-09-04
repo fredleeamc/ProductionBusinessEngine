80e1186c04bb5e17abee84eea037b623d58c9684
Ferdinand Lee <fredlee.amc@gmail.com>
2017-09-04 13:34:51 +0800

    Added WorkCenterType

---
+++ ProductionSchedulerLibrary/ProductionSchedulerLibrary/ProductionSchedulerLibrary/SFC_WorkCenterType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionSchedulerLibrary
+{
+    public class SFC_WorkCenterType
+    {
+        private long id;
+
+        private String workCenterTypeName;
+
+        public SFC_WorkCenterType(long id, string workCenterTypeName)
+        {
+            this.id = id;
+            this.workCenterTypeName = workCenterTypeName;
+        }
+
+        public long Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string WorkCenterTypeName
+        {
+            get
+            {
+                return workCenterTypeName;
+            }
+
+            set
+            {
+                workCenterTypeName = value;
+            }
+        }
+    }
+}
